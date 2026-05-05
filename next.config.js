1b8439dd3f8eae06fa2b6605dee2d9b7c0ece365
AVA <ava@nips.im>
2026-05-05 20:19:13 +0000

    fix: Remove basePath, fix Next.js 16 config, add deploy branch

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/ecobio-nextjs-ui',
   images: {
     unoptimized: true,
   },
-  // Experimental: Force static export to work properly
-  experimental: {
-    outputFileTracingExcludes: {
-      '*': ['./node_modules/@swc/core-linux-x64-gnu'],
-    },
+  outputFileTracingExcludes: {
+    '*': ['./node_modules/@swc/core-linux-x64-gnu'],
   },
 };
 
